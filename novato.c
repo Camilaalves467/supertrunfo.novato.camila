df0bd7d4e15a12e85e84c4fae02f307a72a2dc17
Camilaalves467 <202503032467@alunos.estacio.br>
2025-03-02 21:12:23 +0000

    finalizado nivel aventureiro

--- novato.c
+++ novato.c
@@ -13,39 +13,48 @@ int main (){
     double habitantes2 = 10.546;
     int pontosturisticos = 15;
     int pontosturisticos2 = 7;
-    float pib = 13.365; 
-    float pib2 = 668.985;
+    float pib = 13.985;
+    float pib2 = 14.256;
     float area = 323.92;
     float area2 = 342.568;
+    float resultado = habitantes / area;
 
 
 
     //mostrando informações em tela
-    printf ("Comparando cartas 1 e 2 - \n");
+    printf ("Super Trunfo: carta 1 e 2");
     
-    printf ("Estados \n");
-    printf ("Estado carta 1: %s\n", estado);
-    printf ("Estado carta 2: %s\n", estado2);
+    printf ("*** Carta 1 *** \n");
+    printf ("Estado: %s\n", estado);
 
-    printf ("Cidades \n");
-    printf ("Cidade carta 1: %s\n", cidade);
-    printf ("Cidade carta 2: %s\n", cidade2);
+    printf ("Cidade: %s\n", cidade);
+    
+    printf ("Habitantes: %.3f\n", habitantes);
 
-    printf ("Número de habitantes na cidade \n");
-    printf ("Habitantes carta 1: %f\n", habitantes);
-    printf ("Habitantes carta 2: %f\n", habitantes2);
+    printf ("Pontos turisticos: %d\n", pontosturisticos);
 
-    printf ("Pontos turisticos \n");
-    printf ("Carta 1: %d\n", pontosturisticos);
-    printf ("Carta 2: %d\n", pontosturisticos2);
+    printf ("PIB: %2.f bilhões\n", pib);
 
-    printf ("PIB per capita \n");
-    printf ("PIB carta 1: %2.f bilhões\n", pib);
-    printf ("PIB carta 2: %2.f milhões\n", pib2);
+    printf ("Área em km²: %2.fkm² \n", area);
 
-    printf ("Área em Km² \n");
-    printf ("Área total da cidade carta 1: %2.fkm² \n", area);
-    printf ("Área total da cidade carta 2: %2.fkm² \n", area2);
+    printf ("*** Carta 2 *** \n");
+    printf ("Estado: %s\n", estado2);
+
+    printf ("Cidade: %s\n", cidade2);
+    
+    printf ("Habitantes: %.3f\n", habitantes2);
+
+    printf ("Pontos turisticos: %d\n", pontosturisticos2);
+
+    printf ("PIB: %2.f milhões\n", pib2);
+
+    printf ("Área em km²: %2.fkm² \n", area2);
+
+    printf ("Densidade populacional carta 1: %.3fkm²\n", habitantes / area);
+    printf ("Pib per capita carta 1: %.4f mil\n", pib + habitantes);
+
+    printf ("Densidade populacional carta 2: %.3fkm²\n", habitantes2 / area2);
+    printf ("Pib per capita carta 2: %.4f mil\n", pib2 + habitantes2);
 
     return 0;
 
