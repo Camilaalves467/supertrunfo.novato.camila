cde38c9252860335c39e9a3c1682651df32abc33
Camilaalves467 <202503032467@alunos.estacio.br>
2025-03-04 19:04:49 +0000

    finalizado desafio super trunfo

--- novato.c
+++ novato.c
@@ -5,32 +5,46 @@ int main (){
 
     //informações cartas 1 e 2 já estipuladas
 
-    char estado[20];
+    char estado[20]; 
     char estado2[20];
     char cidade[50];
     char cidade2[50];
     double habitantes = 325.125;
-    double habitantes2 = 10.546;
+    double habitantes2 = 365.478;
     int pontosturisticos = 15;
-    int pontosturisticos2 = 7;
-    float pib = 13.985;
-    float pib2 = 14.256;
-    float area = 323.92;
-    float area2 = 342.568;
-    float resultado = habitantes / area;
+    int pontosturisticos2 = 19;
+    double pib = 13.985;
+    double pib2 = 14.256;
+    double area = 323.92;
+    double area2 = 342.568;
+    float densidadepopu = habitantes / area;
+    float densidadepopu2 = habitantes2 / area2;
+    double pibpercap = pib + area;
+    double pibpercap2 = pib2 + area2;
+    float superpoder = habitantes + pontosturisticos + pib + area + densidadepopu + pibpercap;
+    float superpoder2 = habitantes2 + pontosturisticos2 + pib2 + area2 + densidadepopu2 + pibpercap2;
 
-
-
-    //mostrando informações em tela
-    printf ("Super Trunfo: carta 1 e 2");
     
-    printf ("*** Carta 1 *** \n");
-    printf ("Estado carta 1: %s\n", estado);
+    //mostrando informações em tela
+    printf ("Super Trunfo: carta 1 e 2\n");
+    
+    printf ("*** Carta 1 *** \n\n");
+    printf ("Digite estado e cidade carta 1: \n");
+
+    printf ("Estado: %s\n", estado);
     scanf ("%s", estado);
 
-    printf ("Cidade carta 2: %s\n", cidade);
-    scanf ("%s", cidade);
-    
+    printf ("Cidade: %s\n", cidade);
+    scanf ("%s", &cidade);
+
+    //testando impressão das informações
+    // mostrando informações completas
+
+    printf ("***Dados Carta 1***\n");
+    printf ("Estado: %s\n", estado);
+
+    printf ("Cidade: %s\n", cidade);
+
     printf ("Habitantes: %.3f\n", habitantes);
 
     printf ("Pontos turisticos: %d\n", pontosturisticos);
@@ -38,8 +52,23 @@ int main (){
     printf ("PIB: %2.f bilhões\n", pib);
 
     printf ("Área em km²: %2.fkm² \n", area);
+    
+    printf ("Densidade populacional: %.3fhab/km²\n", densidadepopu);
 
-    printf ("*** Carta 2 *** \n");
+    printf ("Pib per capita: %.3f mil\n\n", pibpercap);
+
+    printf ("*** Carta 2 *** \n\n");
+    printf ("Digite estado e cidade carta 2: \n");
+
+    printf ("Estado: %s\n", estado2);
+    scanf ("%s", &estado2);
+
+    printf ("Cidade: %s\n", cidade2);
+    scanf ("%s", &cidade2);
+
+    //testando impressão das informações
+    // mostrando informações completas
+
     printf ("Estado: %s\n", estado2);
 
     printf ("Cidade: %s\n", cidade2);
@@ -52,15 +81,25 @@ int main (){
 
     printf ("Área em km²: %2.fkm² \n", area2);
 
-    printf ("Densidade populacional carta 1: %.3fhab/km²\n", habitantes / area, (habitantes / area));
-    printf ("Pib per capita carta 1: %.3f mil\n", pib + habitantes, (pib + habitantes));
+    printf ("Densidade populacional carta 2: %.3fhab/km²\n", densidadepopu2);
 
-    printf ("Densidade populacional carta 2: %.3fhab/km²\n", habitantes2 / area2, (habitantes2 / area2));
-    printf ("Pib per capita carta 2: %.3f mil\n", pib2 + habitantes2, (pib2 + habitantes2));
+    printf ("Pib per capita carta 2: %.3f mil\n\n", pibpercap2);
+
+    printf ("Resultados das cartas ganhadoras! \n\n");
+    
+    printf ("Em Habitantes ganhou a Carta %d\n", (habitantes > habitantes2)*1 + (habitantes <= habitantes2) * 2);
+    printf ("Em Pontos Turisticos ganho a Carta %i\n", (pontosturisticos > pontosturisticos2) * 1 + (pontosturisticos <= pontosturisticos2) * 2);
+    printf ("Em PIB ganhou a Carta %d\n", (pib > pib2) * 1 + (pib <= pib2));
+    printf ("Em Área km² ganhou a carta: %d\n", (area > area2) * 1 + (area <= area2) * 2 );
+    printf ("Pela Densidade Populacional ganhou a carta: %d\n", (densidadepopu < densidadepopu2) * 1 + (densidadepopu >= densidadepopu2) * 2);
+    printf ("Pelo PIB per capita, ganhou a carta: %d\n", (pibpercap > pibpercap2) * 1 + (pibpercap <= pibpercap2) * 2);
+    printf ("E o Super Poder ganhador é a carta: %d\n", (superpoder > superpoder2) * 1 + (superpoder <= superpoder2) * 2);
+
+
+
 
     return 0;
 
-    printf ("Agora vamos comparar as Cartas!");
 
     
 
